8ba34f33328ce200dc74317317e080af4e39ba05
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-08-12 17:00:48 +0300

    add: markup

--- src/index.js
+++ src/index.js
@@ -2,12 +2,29 @@ import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
 import SlimSelect from 'slim-select';
 
 const selectBreed = document.querySelector('.breed-select');
+const container = document.querySelector('.cat-info');
 
-fetchBreeds()
-  .tnen(breeds => {
-    createMarkupBreedsSelect(breeds);
-  })
-  .catch(err => console.error(err));
+selectBreed.addEventListener('change', onCatBreedSelect);
+
+function onCatBreedSelect(evt) {
+  container.innerHTML = '';
+
+  const breedOption = fetchCatByBreed(evt.target.value);
+
+  breedOption
+    .then(data => createMarkupCard(data))
+    .catch(err => console.error(err));
+}
+
+function createMarkupCard(data) {
+  const markupSelectCat = el => {
+    return `<h1>${el.breeds[0].name}</h1>
+    <p><strong>Description: </strong>${el.breeds[0].description}</p>
+    <p><strong>Temperament: </strong>${el.breeds[0].temperament}</p>
+    <img src="${el.url}" alt="${el.breeds[0].name}"></img>`;
+  };
+  container.innerHTML = markupSelectCat(data[0]);
+}
 
 function createMarkupBreedsSelect(breeds) {
   const markupSelectBreeds = breeds
@@ -17,8 +34,16 @@ function createMarkupBreedsSelect(breeds) {
     .join(' ');
 
   selectBreed.innerHTML = markupSelectBreeds;
+
   selectBreed.style.visibility = 'inherit';
-  new SlimSelect({
-    select: '.breed-select',
-  });
+  // new SlimSelect({
+  //   select: '.breed-select',
+  // });
+  console.log(breeds);
 }
+
+fetchBreeds()
+  .then(breeds => {
+    createMarkupBreedsSelect(breeds);
+  })
+  .catch(err => console.error(err));
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -7,21 +7,12 @@ axios.defaults.headers.common['x-api-key'] = API_KEY;
 
 export function fetchBreeds() {
   const url = 'https://api.thecatapi.com/v1/breeds';
-  return fetch(`${url}`).then(response => {
-    if (!response.ok) {
-      throw new Error(resp.statusText);
-    }
-    console.log('kvfklfnvklnfvnk');
-    // return response.json();
-  });
+
+  return axios.get(url).then(responce => responce.data);
 }
 
 export function fetchCatByBreed(breedId) {
   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
-  return fetch(`${url}`).then(response => {
-    if (!response.ok) {
-      throw new Error(resp.statusText);
-    }
-    // return response.json();
-  });
+
+  return axios.get(url).then(responce => responce.data);
 }
